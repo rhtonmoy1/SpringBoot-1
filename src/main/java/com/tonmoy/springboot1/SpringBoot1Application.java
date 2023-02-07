9a2e78d5a637134235d4e324868b2a37a08057ce
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2023-02-07 15:11:57 +0600

    Update SpringBoot1Application.java

--- src/main/java/com/tonmoy/springboot1/SpringBoot1Application.java
+++ src/main/java/com/tonmoy/springboot1/SpringBoot1Application.java
@@ -2,8 +2,10 @@ package com.tonmoy.springboot1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.tonmoy.Springboot1")
 public class SpringBoot1Application {
 
     public static void main(String[] args) {
