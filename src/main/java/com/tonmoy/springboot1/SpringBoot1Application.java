c3fa3d80e869e24736b6fd6d9ff5042e7eb1d6af
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2023-02-07 17:44:51 +0600

    g

--- src/main/java/com/tonmoy/springboot1/SpringBoot1Application.java
+++ src/main/java/com/tonmoy/springboot1/SpringBoot1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.tonmoy.Springboot1")
+@ComponentScan("com.tonmoy")
 public class SpringBoot1Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tonmoy/springboot1/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.tonmoy.springboot1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class HelloWorldController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String PrintHello(){
+        return  "Hello World";
+    }
+}
